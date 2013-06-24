b0909e089dea1792a524e506f8edec7285ad0879
Jon Burgess <jkburges@gmail.com>
2013-06-24 21:55:38 +1000

    Partway through generating spec runner from GSP.

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,8 +1,8 @@
 ant.mkdir(dir:"${basedir}/test/jasmine")
-ant.mkdir(dir:"${basedir}/src/jasmine")
+//ant.mkdir(dir:"${basedir}/src/jasmine")
 
 // Copy required javascript files.
-ant.copy(todir: "${basedir}/src/jasmine")
-{
-	fileset(dir: "${pluginBasedir}/src/jasmine")
-}
+//ant.copy(todir: "${basedir}/src/jasmine")
+//{
+//	fileset(dir: "${pluginBasedir}/src/jasmine")
+//}
