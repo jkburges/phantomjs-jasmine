3fb3aa055fdd653a66c178b8bc6263d88637f03c
Jon Burgess <jkburges@gmail.com>
2015-04-09 16:53:01 +1000

    Generate spec runner when running test-app

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,13 +1,17 @@
 loadJasmineTestTypeClass = { ->
-    includeTargets << grailsScript("_GrailsCompile")
-    compile()
+    includeTargets << grailsScript("GenerateSpecRunner")
+    generateSpecRunner()
+
     classLoader.loadClass('grails.plugin.jasmine.JasmineTestType')
 }
 
 loadJasmineTestTypes = {
-    if (!binding.variables.containsKey("unitTests")) return
+    if (!binding.variables.containsKey("unitTests")) {
+        return
+    }
+
     def jasmineTestTypeClass = loadJasmineTestTypeClass()
-    //    [unit: unitTests, integration: integrationTests].each { name, types ->
+
     [unit: unitTests].each { name, types ->
         if (!types.any { it.class == jasmineTestTypeClass }) {
             types << jasmineTestTypeClass.newInstance(name)
